e61ad069e1dea7137246f6b5b96749a5e4b018a9
Tomasz <bosik27@gmail.com>
2021-07-19 13:44:01 +0200

    Init project

---
+++ src/redux/store.js
@@ -0,0 +1,33 @@
+import {combineReducers, createStore} from 'redux';
+//import initialStoreData from '../data/dataStore';
+//import cardsReducer from './cardsRedux';
+
+// define initial state and shallow-merge initial data
+
+const initialState = {
+  //cards: initialStoreData.cards,
+};
+
+// define reducers
+const reducers = {
+  //columns: columnsReducer,
+};
+
+// add blank reducers for initial state properties without reducers
+Object.keys(initialState).forEach(item => {
+  if (typeof reducers[item] == 'undefined') {
+    reducers[item] = (statePart = null) => statePart;
+  }
+});
+
+// merge all reducers
+const storeReducer = combineReducers(reducers);
+
+// create store
+const store = createStore(
+  storeReducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
